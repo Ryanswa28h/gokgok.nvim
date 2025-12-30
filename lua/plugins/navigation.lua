5c72d5a25ba20e48fc5ab280d41e2fce87362f27
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2025-12-30 22:32:11 +0700

    edited flash

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -2,16 +2,8 @@ return {
 	{
 		"folke/flash.nvim",
 		event = "VeryLazy",
-		opts = {
-			modes = {
-				search = {
-					enabled = true,
-				},
-				char = {
-					enabled = true,
-				},
-			},
-		},
+		---@type Flash.Config
+		opts = {},
 		keys = {
 			{
 				"s",
@@ -19,6 +11,7 @@ return {
 				function()
 					require("flash").jump()
 				end,
+				desc = "Flash",
 			},
 			{
 				"S",
@@ -26,6 +19,31 @@ return {
 				function()
 					require("flash").treesitter()
 				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
 			},
 		},
 	},
