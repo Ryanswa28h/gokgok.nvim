a9b70c3c2f46a1729623660024e09362dc37a51c
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2025-12-30 10:32:49 +0700

    Changed keybinds

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -13,13 +13,13 @@ return {
 		vim.o.autoread = true
 
 		-- Recommended/example keymaps.
-		vim.keymap.set({ "n", "x" }, "<C-a>", function()
+		vim.keymap.set({ "n", "x" }, "<leader>oa", function()
 			require("opencode").ask("@this: ", { submit = true })
 		end, { desc = "Ask opencode" })
-		vim.keymap.set({ "n", "x" }, "<C-x>", function()
+		vim.keymap.set({ "n", "x" }, "<leader>ox", function()
 			require("opencode").select()
 		end, { desc = "Execute opencode action…" })
-		vim.keymap.set({ "n", "t" }, "<C-.>", function()
+		vim.keymap.set({ "n", "t" }, "<leader>.", function()
 			require("opencode").toggle()
 		end, { desc = "Toggle opencode" })
 
