eea152132ef236fc73a8ccf57589cbdab4ca1493
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2025-12-30 22:02:26 +0700

    Fixed tmux integration not working with neovim terminal types

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,16 +1,29 @@
 return {
 	"christoomey/vim-tmux-navigator",
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-	},
-	keys = {
-		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-	},
+	config = function()
+		vim.api.nvim_set_keymap("n", "<C-h>", [[<cmd>TmuxNavigateLeft<cr>]], { silent = true, noremap = true })
+		vim.api.nvim_set_keymap("n", "<C-j>", [[<cmd>TmuxNavigateDown<cr>]], { silent = true, noremap = true })
+		vim.api.nvim_set_keymap("n", "<C-k>", [[<cmd>TmuxNavigateUp<cr>]], { silent = true, noremap = true })
+		vim.api.nvim_set_keymap("n", "<C-l>", [[<cmd>TmuxNavigateRight<cr>]], { silent = true, noremap = true })
+
+		vim.api.nvim_set_keymap(
+			"t",
+			"<C-h>",
+			[[<C-\><C-n><cmd>TmuxNavigateLeft<cr>]],
+			{ silent = true, noremap = true }
+		)
+		vim.api.nvim_set_keymap(
+			"t",
+			"<C-j>",
+			[[<C-\><C-n><cmd>TmuxNavigateDown<cr>]],
+			{ silent = true, noremap = true }
+		)
+		vim.api.nvim_set_keymap("t", "<C-k>", [[<C-\><C-n><cmd>TmuxNavigateUp<cr>]], { silent = true, noremap = true })
+		vim.api.nvim_set_keymap(
+			"t",
+			"<C-l>",
+			[[<C-\><C-n><cmd>TmuxNavigateRight<cr>]],
+			{ silent = true, noremap = true }
+		)
+	end,
 }
